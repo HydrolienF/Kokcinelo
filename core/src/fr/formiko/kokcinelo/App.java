db10666d90c5b90d7aa46f8e7c740f0bdbddd8d9
HydrolienF <hydrolien.f@gmail.com>
2022-10-06 20:02:49 +0200

    Add comments

--- core/src/fr/formiko/kokcinelo/App.java
+++ core/src/fr/formiko/kokcinelo/App.java
@@ -1,26 +1,44 @@
 package fr.formiko.kokcinelo;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.ScreenUtils;
 
+/**
+ * {@summary Main class that represent the App &#38; call other Screens.}
+ * It call MainMenuScreen
+ * @see com.badlogic.gdx.Game
+ */
 public class App extends Game {
 	SpriteBatch batch;
+	Screen currentScreen;
 
+	/**
+	 * {@summary Main function call when App is created}
+	 * It call MainMenuScreen
+	 * @see com.badlogic.gdx.ApplicationListener#create()
+	 */
 	@Override
 	public void create() {
+		// TODO add MainMenuScreen that will be call 1st & call GameMenu after
 		batch = new SpriteBatch();
-		GameScreen gs = new GameScreen(this);
-		this.setScreen(gs);
-		// this.setScreen(new MainMenuScreen(this));
+		currentScreen = new GameScreen(this);
+		this.setScreen(currentScreen);
 	}
 
+	/**
+	 * {@summary call render function of sub screens}
+	 * @see com.badlogic.gdx.Game#render()
+	 */
 	@Override
 	public void render() {
-		ScreenUtils.clear(1, 0, 0, 1);
 		super.render();
 	}
 
+	/**
+	 * {@summary call dispose function of item that need to be dispose so that App close itself.}
+	 * @see com.badlogic.gdx.Game#dispose()
+	 */
 	@Override
 	public void dispose() {
 		batch.dispose();
--- core/src/fr/formiko/kokcinelo/GameScreen.java
+++ core/src/fr/formiko/kokcinelo/GameScreen.java
@@ -16,6 +16,11 @@ import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
+/**
+ * {*@summary The action game screen.}
+ * 
+ * @see com.badlogic.gdx.Screen
+ */
 public class GameScreen implements Screen {
     private final App game;
     private Viewport viewport;
@@ -28,16 +33,24 @@ public class GameScreen implements Screen {
     private float rotationSpeed;
     private float maxZoom;
 
+    /**
+     * {*@summary The action game screen constructor that load images &#39; set
+     * Creatures locations.}
+     * 
+     * @param game the App where the Screen is display
+     */
     public GameScreen(final App game) {
+        // TODO move to GameState.java the state of current game
         this.game = game;
-        Gdx.input.setCursorCatched(true);
+        // Gdx.input.setCursorCatched(true);
         float w = Gdx.graphics.getWidth();
-		float h = Gdx.graphics.getHeight();
+        float h = Gdx.graphics.getHeight();
         camera = new OrthographicCamera(30, 30 * (h / w));
-        camera.position.set(Gdx.graphics.getWidth()*0.5f, Gdx.graphics.getHeight()*0.5f, 0);
+        camera.position.set(Gdx.graphics.getWidth() * 0.5f, Gdx.graphics.getHeight() * 0.5f, 0);
         // camera.setToOrtho(false, 800, 480);
         viewport = new ScreenViewport(camera);
         ladybugImage = new Texture(Gdx.files.internal("images/ladybug.png"));
+        ladybugImage.setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
         ladybug = new Rectangle();
         ladybug.width = Gdx.graphics.getWidth() / 10;
         // keep racio
@@ -48,6 +61,7 @@ public class GameScreen implements Screen {
 
         aphidSet = new HashSet<Rectangle>();
         aphidImage = new Texture(Gdx.files.internal("images/aphid.png"));
+        aphidImage.setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
         Random ran = new Random();
         for (int i = 0; i < 10; i++) {
             Rectangle aphid = new Rectangle();
@@ -65,20 +79,28 @@ public class GameScreen implements Screen {
         maxZoom = 0.2f;
     }
 
-    public Camera getCamera(){ return camera;}
+    /**
+     * @return the camera
+     */
+    public Camera getCamera() {
+        return camera;
+    }
 
     @Override
     public void show() {
         // TODO Auto-generated method stub
-
     }
 
+    /**
+     * {@summary Draw all thing that need to be draw during a game.}
+     * @see com.badlogic.gdx.Screen#render(float)
+     */
     @Override
     public void render(float delta) {
         // clear the screen with a green color.
         ScreenUtils.clear(0.1f, 1f, 0f, 1);
         viewport.apply();
-		handleInput();
+        handleInput();
         camera.update();
         game.batch.begin();
         game.batch.setProjectionMatrix(camera.combined);
@@ -87,15 +109,17 @@ public class GameScreen implements Screen {
             game.batch.draw(aphidImage, aphid.x, aphid.y, aphid.width, aphid.height);
         }
         game.batch.draw(ladybugImage, ladybug.x, ladybug.y, ladybug.width, ladybug.height);
-		game.batch.end();
+        game.batch.end();
     }
 
+    /**
+     * {@summary Handle user input &#38; mostly move camera.}<br>
+     * Some input are handle on InputCore that allow more input handling.
+     */
     private void handleInput() {
         // DOING
         InputProcessor inputProcessor = (InputProcessor) new InputCore(this);
-		Gdx.input.setInputProcessor(inputProcessor);
-
-
+        Gdx.input.setInputProcessor(inputProcessor);
 
         // while debuging game close on escape
         if (Gdx.input.isKeyPressed(Input.Keys.ESCAPE))
@@ -116,25 +140,29 @@ public class GameScreen implements Screen {
             moveX += Gdx.graphics.getDeltaTime();
         }
         if (Gdx.input.isKeyPressed(Input.Keys.B)) {
-			camera.rotate(-rotationSpeed, 0, 0, 1);
-		}
-		if (Gdx.input.isKeyPressed(Input.Keys.G)) {
-			camera.rotate(rotationSpeed, 0, 0, 1);
-		}
-        if(camera.zoom<maxZoom){
-            camera.zoom=maxZoom;
+            camera.rotate(-rotationSpeed, 0, 0, 1);
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.G)) {
+            camera.rotate(rotationSpeed, 0, 0, 1);
+        }
+        if (camera.zoom < maxZoom) {
+            camera.zoom = maxZoom;
         }
 
-        moveY *=200;
-        moveX *=200;
+        moveY *= 200;
+        moveX *= 200;
         // if(moveX!=0)
-        //     System.out.println(camera.position);
+        // System.out.println(camera.position);
         // tell the camera to update its matrices.
-        camera.translate((int)moveX, (int)moveY, 0);
-        ladybug.x = camera.position.x-ladybug.width/2;
-        ladybug.y = camera.position.y-ladybug.height/2;
+        camera.translate((int) moveX, (int) moveY, 0);
+        ladybug.x = camera.position.x - ladybug.width / 2;
+        ladybug.y = camera.position.y - ladybug.height / 2;
     }
 
+    /**
+     * {@summary Resize ViewPort when Screen is resize.}
+     * @see com.badlogic.gdx.Screen#resize(int, int)
+     */
     @Override
     public void resize(int width, int height) {
         viewport.update(width, height);
