0fb2bceb68fcb71cf5f60d8e3d49c4aaabb52db5
HydrolienF <hydrolien.f@gmail.com>
2022-10-07 17:18:19 +0200

    fix some space per tab issues in files.

--- core/src/fr/formiko/kokcinelo/App.java
+++ core/src/fr/formiko/kokcinelo/App.java
@@ -11,47 +11,47 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
  * @see com.badlogic.gdx.Game
  */
 public class App extends Game {
-	SpriteBatch batch;
-	Screen currentScreen;
+    SpriteBatch batch;
+    Screen currentScreen;
 
-	/**
-	 * {@summary Main function call when App is created}
-	 * It call MainMenuScreen
-	 * 
-	 * @see com.badlogic.gdx.ApplicationListener#create()
-	 */
-	@Override
-	public void create() {
-		// TODO add MainMenuScreen that will be call 1st & call GameMenu after
-		batch = new SpriteBatch();
-		currentScreen = new GameScreen(this);
-		this.setScreen(currentScreen);
-	}
+    /**
+     * {@summary Main function call when App is created}
+     * It call MainMenuScreen
+     * 
+     * @see com.badlogic.gdx.ApplicationListener#create()
+     */
+    @Override
+    public void create() {
+        // TODO add MainMenuScreen that will be call 1st & call GameMenu after
+        batch = new SpriteBatch();
+        currentScreen = new GameScreen(this);
+        this.setScreen(currentScreen);
+    }
 
-	/**
-	 * {@summary call render function of sub screens}
-	 * 
-	 * @see com.badlogic.gdx.Game#render()
-	 */
-	@Override
-	public void render() {
-		super.render();
-	}
+    /**
+     * {@summary call render function of sub screens}
+     * 
+     * @see com.badlogic.gdx.Game#render()
+     */
+    @Override
+    public void render() {
+        super.render();
+    }
 
-	/**
-	 * {@summary call dispose function of item that need to be dispose so that App
-	 * close itself.}
-	 * 
-	 * @see com.badlogic.gdx.Game#dispose()
-	 */
-	@Override
-	public void dispose() {
-		batch.dispose();
-		// try {
-		// batch.end();
-		// batch.dispose();
-		// } catch (Exception e) {
-		// System.out.println(e);
-		// }
-	}
+    /**
+     * {@summary call dispose function of item that need to be dispose so that App
+     * close itself.}
+     * 
+     * @see com.badlogic.gdx.Game#dispose()
+     */
+    @Override
+    public void dispose() {
+        batch.dispose();
+        // try {
+        // batch.end();
+        // batch.dispose();
+        // } catch (Exception e) {
+        // System.out.println(e);
+        // }
+    }
 }
