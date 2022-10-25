d93c604489299a6206c942c947e35b76add14f5a
HydrolienF <hydrolien.f@gmail.com>
2022-10-25 14:26:36 +0200

    add Hud to implement for text to be display same way even when cam move.

---
+++ core/src/fr/formiko/kokcinelo/view/Hud.java
@@ -0,0 +1,16 @@
+package fr.formiko.kokcinelo.view;
+
+import com.badlogic.gdx.utils.Disposable;
+
+// TODO implement Hud as https://github.com/BrentAureli/SuperMario/blob/dc48dcb6d5b25e8b3c22908df23a5328f13868f9/core/src/com/brentaureli/mariobros/Scenes/Hud.java
+// & then add label that we need from GameState here
+
+public class Hud implements Disposable {
+
+    @Override
+    public void dispose() {
+        // TODO Auto-generated method stub
+        
+    }
+    
+}
