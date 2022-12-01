a3b00f215457224d98da4ce6d73c86f09ea3c416
HydrolienF <hydrolien.f@gmail.com>
2022-12-01 12:37:54 +0100

    new version of launcherjar

--- desktop/src/fr/formiko/kokcinelo/DesktopLauncher.java
+++ desktop/src/fr/formiko/kokcinelo/DesktopLauncher.java
@@ -1,5 +1,10 @@
 package fr.formiko.kokcinelo;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.stream.Collectors;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 
@@ -8,11 +13,22 @@ import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
  * {@summary Launch for Windows, MacOs &#38; linux.}
  * 
  * @author Hydrolien
- * @version 0.1
+ * @version 0.2
  * @since 0.1
  */
 public class DesktopLauncher {
 	public static void main(String[] args) {
+		if (args.length > 0 && args[0].replaceAll("-", "").equalsIgnoreCase("version")) {
+			try {
+				InputStream is = new DesktopLauncher().getClass().getClassLoader().getResourceAsStream("version.md");
+				String version = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8)).lines()
+						.collect(Collectors.joining("\n"));
+				System.out.println(version);
+				System.exit(0);
+			} catch (Exception e) {
+				System.out.println("Fail to get version in DesktopLauncher.");
+			}
+		}
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
 		config.setWindowedMode(1920, 1080);
