5ec541a815da1be37bb1196087a20053d22c1560
HydrolienF <hydrolien.f@gmail.com>
2023-02-12 14:48:33 +0100

    Better quality Textures

--- core/src/main/java/fr/formiko/kokcinelo/tools/KTexture.java
+++ core/src/main/java/fr/formiko/kokcinelo/tools/KTexture.java
@@ -5,9 +5,9 @@ import com.badlogic.gdx.graphics.Texture;
 
 public class KTexture extends Texture {
     public KTexture(FileHandle file) {
-        super(file, true);
-        setFilter(Texture.TextureFilter.MipMapLinearNearest, Texture.TextureFilter.Nearest);
-        // super(file);
-        // setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Nearest);
+        // super(file, true);
+        // setFilter(Texture.TextureFilter.MipMapLinearNearest, Texture.TextureFilter.Nearest);
+        super(file);
+        setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Nearest);
     }
 }
